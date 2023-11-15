e0802e8fa309981923180c8fdeeb534dabeae4de
arturbl <artur.m.costa@trustsystems.pt>
2023-11-15 17:15:57 +0000

    bug fix with versions

--- iot_project/mqtt-pub.py
+++ iot_project/mqtt-pub.py
@@ -4,15 +4,31 @@ import time
 broker_hostname = "localhost"
 port = 1883 
 
+
 def on_connect(client, userdata, flags, return_code):
     if return_code == 0:
         print("connected")
     else:
         print("could not connect, return code:", return_code)
 
+
+def send_message(msgArray):
+    msg_count = 0
+    try:
+        while msg_count < len(msgArray):
+            time.sleep(1)
+            result = client.publish(topic, msgArray[msg_count])
+            status = result[0]
+            if status == 0:
+                print("Message " + str(msgArray[msg_count]) + " is published to topic " + topic)
+            else:
+                print("Failed to send message to topic " + topic)
+            msg_count += 1
+    finally:
+        client.loop_stop()
+
 client = mqtt.Client("Client1")
-# client.username_pw_set(username="user_name", password="password") # uncomment if you use password auth
-client.on_connect=on_connect
+client.on_connect = on_connect
 
 client.connect(broker_hostname, port)
 client.loop_start()
@@ -20,23 +36,10 @@ client.loop_start()
 topic = "idc/fitness"
 msg_count = 0
 
-msg = []
-msg.append('[{"model":"fitness-KNN"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
-msg.append('[{"model":"fitness-GNB"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
-msg.append('[{"model":"fitness-SVC"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
-msg.append('[{"model":"fitness-DT"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
-msg.append('[{"model":"fitness-LR"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
-msg.append('[{"model":"fitness-LDA"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]')
+msg = [
+    '[{"model":"training-DT"},{"acceleration_x":0.2650,"acceleration_y":-0.7814,"acceleration_z":-0.0076,"gyro_x":-0.0590,"gyro_y":0.0325,"gyro_z":-2.9296}]'
+]
 
-try:
-    while msg_count < len(msg):
-        time.sleep(1)
-        result = client.publish(topic, msg[msg_count])
-        status = result[0]
-        if status == 0:
-            print("Message " + str(msg[msg_count]) + " is published to topic " + topic)
-        else:
-            print("Failed to send message to topic " + topic)
-        msg_count += 1
-finally:
-    client.loop_stop()
+send_message(msg)
+
+
