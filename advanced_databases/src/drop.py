30558413a1ed1898f73615a7c14de44024730d83
Artur Costa <arturesmavc@gmail.com>
2023-12-01 13:27:30 +0000

    drop.py script refactor

--- advanced_databases/src/drop.py
+++ advanced_databases/src/drop.py
@@ -1,21 +1,7 @@
 import mysql.connector
 from pymongo import MongoClient
 
-MYSQL_CONFIG = {
-    'user': 'admin',
-    'password': 'admin',
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'mysql'
-}
-
-MONGO_CONFIG = {
-    'host': 'localhost',
-    'port': 27017,
-    'username': 'root',
-    'password': 'rootpassword',
-    'authSource': 'admin'
-}
+files = ["benefits", "salaries", "employee_counts", "job_postings", "companies"]
 
 
 def disable_foreign_keys(cursor):
@@ -28,19 +14,13 @@ def enable_foreign_keys(cursor):
 
 def truncate_mysql_tables():
     print("Truncating MySQL tables...")
-
-    mysql_client = mysql.connector.connect(**MYSQL_CONFIG)
+    mysql_client = mysql.connector.connect(user='admin', password='admin', host='localhost', port=3306, database='mysql')
     cursor = mysql_client.cursor()
-
     try:
         disable_foreign_keys(cursor)
-
-        tables = ["benefits", "salaries", "employee_counts", "job_postings", "companies"]
-
-        for table in tables:
+        for table in files:
             query = f"TRUNCATE TABLE {table}"
             cursor.execute(query)
-
     finally:
         enable_foreign_keys(cursor)
         mysql_client.commit()
@@ -50,14 +30,10 @@ def truncate_mysql_tables():
 
 def drop_mongo_collections():
     print("Dropping MongoDB collections...")
-    collections = ["benefits", "salaries", "employee_counts", "job_postings", "companies"]
-
-    mongo_client = MongoClient(f"mongodb://{MONGO_CONFIG['username']}:{MONGO_CONFIG['password']}@{MONGO_CONFIG['host']}:{MONGO_CONFIG['port']}/{MONGO_CONFIG['authSource']}")
-
-    for collection in collections:
-        db = mongo_client[MONGO_CONFIG['authSource']]
+    mongo_client = MongoClient('mongodb://root:rootpassword@localhost:27017/admin')
+    for collection in files:
+        db = mongo_client.admin
         db[collection].drop()
-
     mongo_client.close()
 
 
