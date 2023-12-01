0c83372c6cbe9d80f41739cfb1ebed8586524e17
Artur Costa <arturesmavc@gmail.com>
2023-12-01 01:23:48 +0000

    csv rewind

---
+++ advanced_databases/jobs-positions/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Stop and remove the containers
+docker-compose down
+
+# Remove the images
+docker-compose rm -f
+
+# Prune unused images (optional, use with caution)
+docker image prune -a -f
